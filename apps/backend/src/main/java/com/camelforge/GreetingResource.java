c60f49ab665252d40ecf195186c29b5c3fea147e
subapavel <subapav@gmail.com>
2025-12-09 13:45:08 +0100

    Add basic GreetingResource and configure cors in quarkus

--- apps/backend/src/main/java/com/camelforge/GreetingResource.java
+++ apps/backend/src/main/java/com/camelforge/GreetingResource.java
@@ -5,12 +5,12 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Quarkus REST";
+        return "Hello from Camel Forge Backend!";
     }
 }
